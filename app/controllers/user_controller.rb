9dccf3dfb7b1f2e689214d02c84a34a207ac2994
Guilherme Mataveli <guilherme.mataveli@reactormodel.com>
2024-11-06 21:23:04 -0300

    initial commit

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,23 @@
+class UserController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
